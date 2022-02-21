ee3fbd20dc1b8f78b8bd6f0f244673713f0be443
Ulan <uzhumabaev@megacom.kg>
2022-02-21 18:04:10 +0600

    feat: added search bar for the main chart page

--- src/pages/MainChart/MainChart.js
+++ src/pages/MainChart/MainChart.js
@@ -1,10 +1,29 @@
 import React from "react";
+import styles from "./MainChart.module.css";
 
 export const MainChart = () => {
     return (
-        <div>
-            Имя руководителя
-            Imya rukovoidleia
+        <div className={styles.mainChart}>
+            <div className={styles.searchBar}>
+                <div className={styles.dateFieldBlock}>
+                    <fieldset>
+                        <legend>Дата</legend>
+                        <input type="date"/>
+                    </fieldset>
+                </div>
+                <div className={styles.searchFieldBlock}>
+                    <fieldset>
+                        <legend>Поиск</legend>
+                        <input type="text"/>
+                    </fieldset>
+                </div>
+                <div className={styles.buttonBlock}>
+                    <button className="btn btn-main">Сохранить</button>
+                </div>
+                <div className={styles.buttonBlock}>
+                    <button className="btn btn-secondary">Сохранить в excel</button>
+                </div>
+            </div>
         </div>
     )
 };
