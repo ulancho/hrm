f534d263e5f8ad1eae5ee13ba09c95110c563da8
Ulan <uzhumabaev@megacom.kg>
2022-02-22 17:14:47 +0600

    feat: added/styled employee card

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Employees.module.css";
 import {ReactComponent as SearchIcon} from "./../../media/icons/search.svg";
+import userImg from "./user.png";
 
 export const Employees = () => {
     return (
@@ -29,7 +30,77 @@ export const Employees = () => {
                 </div>
             </div>
             <div className={styles.tableBar}>
+                <div className={styles.emplsList}>
 
+                    <div className={styles.emplCard}>
+                        <div className={styles.photo}>
+                            <img src={userImg} alt=""/>
+                        </div>
+                        <div className={styles.labels}>
+                            <ul>
+                                <li>Ф.И.О:</li>
+                                <li>Должность:</li>
+                                <li>Моб. тел.:</li>
+                            </ul>
+                        </div>
+                        <div className={styles.data}>
+                            <ul>
+                                <li>Коробова Виктория Евгеньевна</li>
+                                <li>Старший дизайнер</li>
+                                <li>0555 505 292</li>
+                            </ul>
+                        </div>
+                        <div className={styles.labels}>
+                            <ul>
+                                <li>Внутр. тел.:</li>
+                                <li>Идентификатор 1С:</li>
+                                <li>Эл. почта:</li>
+                            </ul>
+                        </div>
+                        <div className={styles.data}>
+                            <ul>
+                                <li>нет</li>
+                                <li>27391019</li>
+                                <li>vkorobova@megacom.kg</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={styles.emplCard}>
+                        <div className={styles.photo}>
+                            <img src={userImg} alt=""/>
+                        </div>
+                        <div className={styles.labels}>
+                            <ul>
+                                <li>Ф.И.О:</li>
+                                <li>Должность:</li>
+                                <li>Моб. тел.:</li>
+                            </ul>
+                        </div>
+                        <div className={styles.data}>
+                            <ul>
+                                <li>Коробова Виктория Евгеньевна</li>
+                                <li>Старший дизайнер</li>
+                                <li>0555 505 292</li>
+                            </ul>
+                        </div>
+                        <div className={styles.labels}>
+                            <ul>
+                                <li>Внутр. тел.:</li>
+                                <li>Идентификатор 1С:</li>
+                                <li>Эл. почта:</li>
+                            </ul>
+                        </div>
+                        <div className={styles.data}>
+                            <ul>
+                                <li>нет</li>
+                                <li>27391019</li>
+                                <li>vkorobova@megacom.kg</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
