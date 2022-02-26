4fb790596825d4b5538d6ddda2b84b3e594172de
Ulan <uzhumabaev@megacom.kg>
2022-02-26 17:49:05 +0600

    fix: added styles to modules

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import styles from './Modal.module.css';
 
 
 /**************** фон для модалки *****************/
@@ -7,6 +8,6 @@ export const BackdropModal = () => {
     const state = useSelector(state => state.modal.backdropModal);
 
     return (
-        <div className={`modal-backdrop ${state}`}/>
+        <div className={`${styles.modalBackdrop} ${styles[state]}`}/>
     )
 };
