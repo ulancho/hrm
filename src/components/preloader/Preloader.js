4183a364ac7ac1ae8ed364d6ad1187ccf70cae1a
Ulan <uzhumabaev@megacom.kg>
2022-02-26 17:19:29 +0600

    fix: added styles to modules

--- src/components/preloader/Preloader.js
+++ src/components/preloader/Preloader.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import styles from './Preloader.module.css'
 
 export const Preloader = () => {
     const state = useSelector(state => state.modal.preloader);
 
     return (
-        <div className={`loader ${state}`}>
-            <div className="outer"/>
-            <div className="middle"/>
-            <div className="inner"/>
+        <div className={`${styles.loader} ${styles[state]}`}>
+            <div className={styles.outer}/>
+            <div className={styles.middle}/>
+            <div className={styles.inner}/>
         </div>
     )
 };
