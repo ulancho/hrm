c59e23eeb4fe9283b1248b2ca144a8aa1eac3483
Ulan <uzhumabaev@megacom.kg>
2022-02-28 18:27:33 +0600

    feat: changed to new design

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import "normalize.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {MainChart} from "./pages/MainChart/MainChart";
-import {SideBar} from "./components/sideBar/SideBar";
 import {MainContent} from "./components/mainContent/MainContent";
 import {Profile} from "./components/profile/Profile";
 import {Employees} from "./pages/Employees/Employees";
 import {Preloader} from "./components/preloader/Preloader";
 import {BackdropModal} from "./components/modal/Modal";
+import {SideBar} from "./components/sideBar/SideBar";
 
 function App() {
   return (
       <BrowserRouter>
+          <Profile/>
           <div className="d-flex">
               <SideBar/>
               <MainContent>
-                  <Profile/>
                   <Routes>
                       <Route path="main_chart" element={<MainChart/>}/> /*стр. основной график*/
                       <Route path="employees" element={<Employees/>}/> /*стр. список сотрудников*/
