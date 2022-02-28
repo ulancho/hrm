9605df5b20312a8ac55b8ef86196f75e94ea9c14
Ulan <uzhumabaev@megacom.kg>
2022-02-28 15:55:19 +0600

    fix: changed profile bar

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Profile.module.css";
 import {ReactComponent as ProfileIcon} from "./../../media/icons/profile.svg";
+import {ReactComponent as Logo} from "../../media/icons/logo.svg";
 
 export const Profile = () => {
     return (
-        <div className={styles.profile}>
-            <ProfileIcon/>
-            <p className={styles.profileName}>Имя руководителя</p>
+        <div className={`container ${styles.profileBar}`}>
+            <div className={styles.logo}>
+                <Logo/>
+            </div>
+            <div className={styles.profile}>
+                <ProfileIcon/>
+                <p className={styles.profileName}>Имя руководителя</p>
+            </div>
         </div>
     )
 };
