f3a8850a6f7c25ea1a72e2fc608797945e603321
Ulan <uzhumabaev@megacom.kg>
2022-03-09 18:57:04 +0600

    fix: jumping scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "normalize.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MainChart} from "./pages/MainChart/MainChart";
 import {MainContent} from "./components/mainContent/MainContent";
 import {Profile} from "./components/profile/Profile";
@@ -10,23 +10,23 @@ import {BackdropModal, ErrorApiModal} from "./components/modal/Modal";
 import {SideBar} from "./components/sideBar/SideBar";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Profile/>
-          <div className="d-flex">
-              <SideBar/>
-              <MainContent>
-                  <Routes>
-                      <Route path="/main_chart" element={<MainChart/>}/> /*стр. основной график*/
-                      <Route path="/employees" element={<Employees/>}/> /*стр. список сотрудников*/
-                  </Routes>
-              </MainContent>
-          </div>
-          <BackdropModal/>
-          <Preloader/>
-          <ErrorApiModal/>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Profile/>
+            <div className="d-flex">
+                <SideBar/>
+                <MainContent>
+                    <Routes>
+                        <Route path="/main_chart" element={<MainChart/>}/> /*стр. основной график*/
+                        <Route path="/employees" element={<Employees/>}/> /*стр. список сотрудников*/
+                    </Routes>
+                </MainContent>
+            </div>
+            <Preloader/>
+            <BackdropModal/>
+            <ErrorApiModal/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
