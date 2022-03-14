2b52b98598969b20eabcd7914cddebb2bfe3e204
Ulan <uzhumabaev@megacom.kg>
2022-03-14 18:48:09 +0600

    feat: activated status

--- src/components/contextMenu/ContextMenu.js
+++ src/components/contextMenu/ContextMenu.js
@@ -1,7 +1,11 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import styles from "./ContextMenu.module.css";
+
 
 export const ContextMenu = () => {
-    const handleClick = () => {
+    const [field, setField] = useState(false);
+
+    const clickCloseContextMenu = () => {
         let menu = document.querySelectorAll('.menu');
         let activeCell = document.querySelectorAll('.active-cell');
         for(let i = 0; i < menu.length; i++){
@@ -12,20 +16,31 @@ export const ContextMenu = () => {
         }
     }
 
-    useEffect(() => {
-        document.addEventListener("click", handleClick);
-        return () => {
-            document.addEventListener("click", handleClick);
-        };
-    });
+    const clickItemStatus = (event,type) => {
+        event.currentTarget.classList.add('active-status');
+        setField(true);
+    }
+
+    const HoursField = () => {
+        return <input className={styles.hoursFiled} type="text" placeholder="Отработанные часы"/>
+
+    }
+
+
+    // useEffect(() => {
+    //     document.addEventListener("click", clickCloseContextMenu);
+    //     return () => {
+    //         document.addEventListener("click", clickCloseContextMenu);
+    //     };
+    // });
 
     return (
         <div className="menu d-none">
             <ul>
-                <li className="s active-status">Отработано</li>
-                <li className="s">Командировочные</li>
-                <li className="s">Больничные</li>
-                <li className="s">Опуск</li>
+                <li onClick={(event)=>clickItemStatus(event,1)} className="s">{field ? <HoursField/>  : 'Отработано'}</li>
+                <li onClick={(event)=>clickItemStatus(event,2)} className="s">Командировочные</li>
+                <li onClick={(event)=>clickItemStatus(event,3)} className="s">Больничные</li>
+                <li onClick={(event)=>clickItemStatus(event,4)} className="s">Опуск</li>
             </ul>
         </div>
     );
