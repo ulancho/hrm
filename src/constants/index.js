c12dd18b324a809356eee656fee3d8043668f7d5
Ulan <uzhumabaev@megacom.kg>
2022-03-22 18:18:29 +0600

    refactor: changed token

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const IMAGE_URL = 'https://reports.megacom.local/plugins/BussinesDirectory/cache/photosEx/';
 export const BASE_URL = 'http://10.242.147.11:8000/';
-export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBtZWdhY29tLmtnIiwiZXhwIjoxNjQ3NjY4NDc2fQ.UMcF3-PFdwK29-PCwLobxJHBPFEzi2n7m_5DTv6DSXI';
+export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBtZWdhY29tLmtnIiwiZXhwIjoxNjQ4Mzc1OTE3fQ.OaSEu4_lLkD37IzuMTVPY-T1dwG8WfGXJ6-XRu5zfeY';
 export const SHIFT_STATUSES = {
     1:{
         name: "Больничный день",
