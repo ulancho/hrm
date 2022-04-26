45b5d1e30c7879b0049108f3b07b65156a1f0c10
Ulan <uzhumabaev@megacom.kg>
2022-04-26 15:26:19 +0600

    feat: login page: added spinner, checked email, activated button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Login.module.css';
 import {ReactComponent as Logo} from "../../media/icons/logo_white.svg";
+import classNames from "classnames";
+import spinner from "../../media/1495.gif";
 
 const Login = () => {
+    const [activeBtn, setActiveBtn] = useState('btn-not-active');
+    const [isPending, setIsPending] = useState(false)
+
+    //проверка на наличие в конце @megacom.kg
+    const checkEmail = (email) => {
+        const regex = /@megacom\.kg$/i;
+        return regex.test(email);
+    }
+
+    const changeEmail = (e) => {
+        if(checkEmail(e.target.value)){
+            setActiveBtn('btn-main');
+        }
+    }
+
+    const clickOnwards = () => {
+        setIsPending(true);
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <Logo className={styles.logo}/>
                 <div className={styles.input_container}>
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="text" placeholder="Введите Email"/>
-                    <button className="btn btn-not-active mt-32">Далее</button>
+                    <input
+                        id="email"
+                        type="text"
+                        placeholder="Введите Email"
+                        onChange={changeEmail}/>
+                    <button
+                        className={classNames('btn mt-32',activeBtn)}
+                        onClick={clickOnwards}>
+                        {!isPending ? 'Далее' : <img className={styles.spinner} src={spinner} alt="...загрузка"/>}
+                    </button>
                 </div>
             </div>
         </div>
