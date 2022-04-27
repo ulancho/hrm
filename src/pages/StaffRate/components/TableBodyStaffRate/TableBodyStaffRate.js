5cc2294ba90aaef2dc033437759c8b5042031700
Ulan <uzhumabaev@megacom.kg>
2022-04-27 16:52:30 +0600

    feat: added for staff rate pagination

--- src/pages/StaffRate/components/TableBodyStaffRate/TableBodyStaffRate.js
+++ src/pages/StaffRate/components/TableBodyStaffRate/TableBodyStaffRate.js
@@ -3,39 +3,45 @@ import styles from "./TableBodyStaffRate.module.css"
 import classNames from "classnames";
 import {auto_grow} from "../../../../helpers";
 
-const TableRow = () => {
+const TableRow = ({item}) => {
     return (
         <div className={styles.tableBody}>
             <div className={classNames(styles.num, styles.border)}>
                 <span>1</span>
             </div>
             <div className={classNames(styles.name, styles.border)}>
-                <p>Абасова Айгерим Толоновна </p>
-                <p>Специалист по поддержке ПО</p>
+                <p>{item.full_name}</p>
+                <p>{item.position}</p>
             </div>
             <div className={classNames(styles.ratio1, styles.border)}>
-                <span className={classNames(styles.ratioNum1)}>0.2</span>
+                <span className={classNames(styles.ratioNum1)}>{item.department_ratio}</span>
             </div>
             <div className={classNames(styles.ratio2, styles.border)}>
-                <span className={classNames(styles.ratioNum1)}>0.2</span>
+                <span className={classNames(styles.ratioNum1)}>{item.efficiency_ratio}</span>
             </div>
             <div className={classNames(styles.comment, styles.border)}>
                 <textarea placeholder="Отчет о проделанной работе за месяц" onBlur={()=>console.log('123')} onInput={auto_grow}/>
             </div>
             <div className={classNames(styles.ratio1, styles.border)}>
-                <span className={classNames(styles.ratioNum1)}>0.2</span>
+                <span className={classNames(styles.ratioNum1)}>{item.revenue_ratio}</span>
             </div>
             <div className={classNames(styles.ratio3, styles.border)}>
-                <span className={classNames(styles.ratioNum2)}>0.2</span>
+                <span className={classNames(styles.ratioNum2)}>{item.total_ratio}</span>
             </div>
         </div>
     )
 }
 
-const TableBodyStaffRate = () => {
+const TableBodyStaffRate = ({items}) => {
     return (
         <div className="animate__animated animate__zoomIn animate__fast">
-            <TableRow/>
+            {
+                items.count > 0 ? items.data.map((item,index)=>{
+                    return (
+                        <TableRow key={index} item={item}/>
+                    )
+                }) : <h3 id="not-found" className="text-center">Данные не найдены</h3>
+            }
         </div>
     );
 };
--- src/pages/StaffRate/components/TableStaffRate/TableStaffRate.js
+++ src/pages/StaffRate/components/TableStaffRate/TableStaffRate.js
@@ -1,20 +1,58 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import TableHeaderStaffRate from "../TableHeaderStaffRate/TableHeaderStaffRate";
 import TableBodyStaffRate from "../TableBodyStaffRate/TableBodyStaffRate";
 import {getStaffRate} from "../../../../redux/actions";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import ReactPaginate from "react-paginate";
 
 const TableStaffRate = () => {
     const dispatch = useDispatch();
+    const items = useSelector(state => state.staff_rate.data);
+    const itemsPerPage = 10;
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    /********************** обработчики для событий ********************/
+    const handlePageClick = (event) => {
+        const newOffset = event.selected * itemsPerPage % items.count;
+        setItemOffset(newOffset);
+    };
+
+    /********************** хуки ********************/
+    useEffect(() => {
+        setPageCount(Math.ceil(items.count / itemsPerPage));
+    }, [items]);
 
     useEffect(() => {
-        dispatch(getStaffRate({limit:10,offset:0}));
-    }, []);
+        dispatch(getStaffRate({limit:itemsPerPage,offset:itemOffset}));
+    }, [itemOffset]);
 
     return (
         <>
             <TableHeaderStaffRate/>
-            <TableBodyStaffRate/>
+            <TableBodyStaffRate items={items}/>
+            {
+                items.count ? <ReactPaginate
+                    previousLabel="Назад"
+                    nextLabel="Следующий"
+                    breakLabel="..."
+                    pageCount={pageCount}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    onPageChange={handlePageClick}
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                /> : null
+            }
         </>
     );
 };
