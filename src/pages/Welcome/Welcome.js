941355617156564840659f4aee43a4b0ae3978d0
Ulan <uzhumabaev@megacom.kg>
2022-06-09 16:38:39 +0600

    refactor: setting welcome page

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,8 +1,24 @@
 import React from "react";
+import {Profile} from "../../components/profile/Profile";
+import {SideBar} from "../../components/sideBar/SideBar";
+import {MainContent} from "../../components/mainContent/MainContent";
+import styles from "../MainChart/MainChart.module.css";
 
 const Welcome = () => {
     return (
-        <h1>Добро пожаловать</h1>
+        <>
+            <Profile/>
+            <div className="d-flex">
+                <SideBar/>
+                <MainContent>
+                    <div className={`${styles.mainChart} animate__animated animate__fadeIn animate__fast`}>
+                        <div className="wrapper">
+                            <h1>Добро пожаловать</h1>
+                        </div>
+                    </div>
+                </MainContent>
+            </div>
+        </>
     )
 }
 
