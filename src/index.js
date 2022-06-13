f3fb7dcd94b755eda0b403b12f13a4d1f2de31e5
Ulan <uzhumabaev@megacom.kg>
2022-06-13 19:00:49 +0600

    refactor: delete devtools extension

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {rootReducer} from "./redux/rootReducer";
+import {rootReducer} from "./reducers/rootReducer";
 import {applyMiddleware, compose, createStore} from "redux";
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
 
 
+// const store = createStore(rootReducer, compose(
+//     applyMiddleware(
+//         thunk
+//     ),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 const store = createStore(rootReducer, compose(
     applyMiddleware(
         thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    )));
 
 ReactDOM.render(
     <Provider store={store}>
