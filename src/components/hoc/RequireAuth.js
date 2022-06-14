ce59ff26fa4d79f846854b1b1e6b37569bba9d71
Ulan <uzhumabaev@megacom.kg>
2022-06-14 17:34:49 +0600

    feat: checking the life of the token

--- src/components/hoc/RequireAuth.js
+++ src/components/hoc/RequireAuth.js
@@ -1,8 +1,19 @@
 import React from "react";
 import {Navigate} from "react-router-dom";
+import {parseJwt} from "../../helpers";
+import AuthService from "../../services/auth.service"
 
 export const RequireAuth = ({children}) => {
     const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user) {
+      const decodedJwt = parseJwt(user.access_token);
+
+      if (new Date() >= new Date(decodedJwt.exp * 1000)) {
+          AuthService.logout();
+          return <Navigate to='/login?status=1'/>
+       }
+    }
 
     if (!user || !user.access_token){
         return <Navigate to='/login'/>
