7793df2ec677bfb857b988e51246fa2f47a4190f
Ulan <uzhumabaev@megacom.kg>
2022-06-20 15:57:24 +0600

    refactor: changed reducers directory

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {rootReducer} from "./reducers/rootReducer";
+import {rootReducer} from "./store/reducers/rootReducer";
 import {applyMiddleware, compose, createStore} from "redux";
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
