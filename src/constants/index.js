9feb20452aac90818ab0479b36772cfb02df2437
Ulan <uzhumabaev@megacom.kg>
2022-06-23 13:50:32 +0600

    feat: added new image url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const IMAGE_URL = 'https://reports.megacom.local/plugins/BussinesDirectory/cache/photosEx/';
+export const IMAGE_URL = 'http://10.242.147.11:8000/staff/media/';
 export const BASE_URL = 'http://10.242.147.11:8000/';
 export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBtZWdhY29tLmtnIiwiZXhwIjoxNjUxMzc4MjE4fQ.CpdZ6bsiF_TFkEP_G9TN_Pf1arUO1SVOxrB673AdFD0';
 export const SHIFT_STATUSES = {
